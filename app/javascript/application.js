e421fc90b5a893d673330502a498f11b5508974c
Annihilous <benjaminjnorton@gmail.com>
2024-12-23 18:26:08 -0500

    Add datepicker for MROW form.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
-
-//= require flatpickr
-document.addEventListener('DOMContentLoaded', function() {
-  flatpickr('.flatpickr-selector');
-})
